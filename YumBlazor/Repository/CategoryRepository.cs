4d16c65c1ce05a0d71fa6322ad3bef3b5043f050
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-04-04 11:55:34 +0200

    Implement Category Repository

---
+++ YumBlazor/Repository/CategoryRepository.cs
@@ -0,0 +1,61 @@
+﻿using YumBlazor.Data;
+using YumBlazor.Repository.IRepository;
+
+namespace YumBlazor.Repository;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public CategoryRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
+    public Category Create(Category obj)
+    {
+        _db.Categories.Add(obj);
+        _db.SaveChanges();
+        return obj;
+    }
+
+    public bool Delete(int id)
+    {
+        var obj = _db.Categories.FirstOrDefault(u => u.Id == id);
+
+        if (obj != null)
+        {
+            _db.Categories.Remove(obj);
+            return _db.SaveChanges() > 0;
+        }
+        return false;
+    }
+
+    public Category Get(int id)
+    {
+        var obj = _db.Categories.FirstOrDefault(u => u.Id == id);
+        if (obj == null)
+        {
+            return new Category();
+        }
+        return obj;
+    }
+
+    public Category Update(Category obj)
+    {
+        var objFromDb = _db.Categories.FirstOrDefault(u => u.Id == obj.Id);
+        if (objFromDb != null)
+        {
+            objFromDb.Name = obj.Name;
+            _db.Categories.Update(objFromDb);
+            _db.SaveChanges();
+            return objFromDb;
+        }
+        return obj;
+    }
+
+    public IEnumerable<Category> GetAll()
+    {
+        return _db.Categories.ToList();
+    }
+}
