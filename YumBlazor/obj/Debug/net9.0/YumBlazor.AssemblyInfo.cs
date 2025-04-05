638ae3fa8e5ebd4e4e9322655bf253576ffb878d
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-04-05 11:16:45 +0200

    Added new buttons and new blank blazor component for adding and updating object

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b68325b19911447495361fb36536264ebee19a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2de748ac5b2693821688924f669723869aa0f027")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
