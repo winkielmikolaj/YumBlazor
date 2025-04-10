ff9563f5bf374859679d8fc2a5f1529a9dbfcbb3
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-04-10 11:10:24 +0200

    Dynamic NavBar based on Role of the user

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11667e844a129e2f3139f66c44631c7dc40315b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282e48e4c5fda304a230cdc3490c1dbe229fa239")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
