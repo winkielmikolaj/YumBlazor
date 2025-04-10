192e722b8938baba1f91ce9803fa4946beead3d7
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-04-10 11:17:10 +0200

    Display link based on authorization status

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282e48e4c5fda304a230cdc3490c1dbe229fa239")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff9563f5bf374859679d8fc2a5f1529a9dbfcbb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
