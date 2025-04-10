ac31a34aa06d5ead51d534f892b05f3c0dc83f1c
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-04-10 15:14:59 +0200

    Spinner and loading products on Home page

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22dd3a26c9a4e78fe7e88ee79629f7d8322cc33a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a29ff6300b409bcf1c59edefb9e6404b8dc6931")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
