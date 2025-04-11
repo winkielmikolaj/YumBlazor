7ce8039309352c8bd8b8eb1fec8762f29db72da1
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-04-11 11:14:24 +0200

    Add Item to cart only when user is logged in

--- YumBlazor/Repository/IRepository/IShoppingCartRepository.cs
+++ YumBlazor/Repository/IRepository/IShoppingCartRepository.cs
@@ -4,7 +4,7 @@ namespace YumBlazor.Repository.IRepository;
 
 public interface IShoppingCartRepository
 {
-    public Task<bool> UpdateCartAsync(string userId, int product, int updateBy);
+    public Task<bool> UpdateCartAsync(string? userId, int product, int updateBy);
     public Task<IEnumerable<ShoppingCart>> GetAllAsync(string? userId);
     public Task<bool> ClearCartAsync(string? userId);
 }
