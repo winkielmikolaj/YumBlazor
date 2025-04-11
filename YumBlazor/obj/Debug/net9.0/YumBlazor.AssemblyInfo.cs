d1f1a98ff8cb01323e6e568a692a02db2eee893d
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-04-11 11:38:27 +0200

    Load Shopping Cart

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43bebbbc6a725374c3c3bd101d3b040cd0158c9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ce8039309352c8bd8b8eb1fec8762f29db72da1")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
