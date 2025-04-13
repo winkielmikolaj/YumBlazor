80b1d9e372b76bbbdc305b5bfe98c482c71b81f8
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-04-13 13:58:24 +0200

    Updating Order Status by Admin

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71e8249d66d41ec813ba3dfb7de80b9b5fad25dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaf364d7ebac1731d5c30ba16e2242cda1d16a3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
