4536c5a4b152302eb9fb79238c5988bcfb7e00b0
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-04-14 10:47:54 +0200

    DataGrid in Radzen

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+140770174bd00fdceb7ef80c8430ca16d44131c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a300e2c652535544c64dfb79a5274c7025030850")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
