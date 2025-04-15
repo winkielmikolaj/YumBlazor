df40b80b0a45edb073d178336ca421532c648f2d
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-04-15 16:36:36 +0200

    Repaired NavBar for customer.role

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85a6423a210d5148059801e999c2f150164cfb75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db96b1d37ada0a8eb8633c4cb7d48f1262721334")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
